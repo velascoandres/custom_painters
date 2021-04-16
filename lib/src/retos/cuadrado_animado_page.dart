6222ee9673a278d524e3614c8dcf7be041568437
andres <velasco.andrs@gmail.com>
2021-04-15 23:31:27 -0500

    reto fin

--- lib/src/retos/cuadrado_animado_page.dart
+++ lib/src/retos/cuadrado_animado_page.dart
@@ -12,22 +12,66 @@ class CuadradoAnimadoPage extends StatelessWidget {
 }
 
 class _CuadradoAnimado extends StatefulWidget {
-
   @override
   __CuadradoAnimadoState createState() => __CuadradoAnimadoState();
 }
 
-class __CuadradoAnimadoState extends State<_CuadradoAnimado> with SingleTickerProviderStateMixin {
-
+class __CuadradoAnimadoState extends State<_CuadradoAnimado>
+    with SingleTickerProviderStateMixin {
   AnimationController controller;
   // animaciones
-  // 
+  Animation<double> moverDerecha;
+  Animation<double> moverArriba;
+  Animation<double> moverIzquierda;
+  Animation<double> moverAbajo;
+
   @override
   void initState() {
     controller = AnimationController(
       vsync: this,
-      duration: Duration(milliseconds: 4000),
+      duration: Duration(milliseconds: 4500),
     );
+
+    moverDerecha = Tween(
+      begin: 0.0,
+      end: 150.0,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.0, 0.25, curve: Curves.bounceOut),
+      ),
+    );
+
+    moverArriba = Tween(
+      begin: 0.0,
+      end: -150.0,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.25, 0.50, curve: Curves.bounceOut),
+      ),
+    );
+
+    moverIzquierda = Tween(
+      begin: 0.0,
+      end: -150.0,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.5, 0.75, curve: Curves.bounceOut),
+      ),
+    );
+
+    moverAbajo = Tween(
+      begin: 0.0,
+      end: 150.0,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.75, 1.0, curve: Curves.bounceOut),
+      ),
+    );
+
     super.initState();
   }
 
@@ -37,20 +81,28 @@ class __CuadradoAnimadoState extends State<_CuadradoAnimado> with SingleTickerPr
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return _Rectangulo();
+
+
+    controller.repeat();
+
+    return AnimatedBuilder(
+      animation: controller,
+      child: _Rectangulo(),
+      builder: (BuildContext context, Widget child) {
+        
+        final desplazamientoX = moverDerecha.value + moverIzquierda.value;
+        final desplazamientoY = moverArriba.value + moverAbajo.value;
+        
+        return Transform.translate(
+          offset: Offset(desplazamientoX, desplazamientoY),
+          child: child,
+        );
+      },
+    );
   }
 }
-
-
-
-
-
-
-
-
 
 class _Rectangulo extends StatelessWidget {
   @override
