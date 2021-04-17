025303a4658b895b9d2ec3591279ad19d0fb0c14
Andrés Velasco <velasco.andrs@gmail.com>
2021-04-17 17:39:52 -0500

    animando el arco

--- lib/src/pages/circular_progress_page.dart
+++ lib/src/pages/circular_progress_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -9,15 +10,47 @@ class CircularProgressPage extends StatefulWidget {
   _CircularProgressPageState createState() => _CircularProgressPageState();
 }
 
-class _CircularProgressPageState extends State<CircularProgressPage> {
-  double porcentaje = 10;
+class _CircularProgressPageState extends State<CircularProgressPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  double porcentaje = 0.0;
+  double nuevoPorcentaje = 0.0;
+
+  @override
+  void initState() {
+    controller = new AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 800),
+    );
+
+    controller.addListener(() {
+      porcentaje = lerpDouble(porcentaje, nuevoPorcentaje, controller.value);
+      print(porcentaje);
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () => setState(() {
-          porcentaje > 100 ? porcentaje = 0 : porcentaje += 10;
+          porcentaje = nuevoPorcentaje;
+          nuevoPorcentaje += 10;
+          if (nuevoPorcentaje > 100) {
+            nuevoPorcentaje = 0;
+            porcentaje = 0;
+          }
+
+          controller.forward(from: 0.0);
         }),
         child: Icon(Icons.refresh),
         backgroundColor: Colors.pink,
