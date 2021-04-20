f81065feddb5e84fea3ecbc271f1b131cac4c3e6
andres <velasco.andrs@gmail.com>
2021-04-20 10:49:57 -0500

    pinterest init

--- lib/src/pages/menu_page.dart
+++ lib/src/pages/menu_page.dart
@@ -12,43 +12,8 @@ class MenuPage extends StatelessWidget {
           children: [
             ListTile(
               leading: Icon(Icons.add_box),
-              title: Text('Square'),
-              onTap: this._navigateTo(context, 'square-page'),
-            ),
-            ListTile(
-              leading: Icon(Icons.square_foot),
-              title: Text('Diagonal'),
-              onTap: this._navigateTo(context, 'diagonal-page'),
-            ),
-            ListTile(
-              leading: Icon(Icons.text_rotation_angledown),
-              title: Text('Triangular'),
-              onTap: this._navigateTo(context, 'triangular-page'),
-            ),
-            ListTile(
-              leading: Icon(Icons.square_foot),
-              title: Text('Pico'),
-              onTap: this._navigateTo(context, 'pico-page'),
-            ),
-            ListTile(
-              leading: Icon(Icons.rounded_corner),
-              title: Text('Circular'),
-              onTap: this._navigateTo(context, 'circular-page'),
-            ),
-            ListTile(
-              leading: Icon(Icons.add_circle_outline),
-              title: Text('Curve'),
-              onTap: this._navigateTo(context, 'curve-page'),
-            ),
-            ListTile(
-              leading: Icon(Icons.waves),
-              title: Text('Wave'),
-              onTap: this._navigateTo(context, 'wave-page'),
-            ),
-            ListTile(
-              leading: Icon(Icons.waves_sharp),
-              title: Text('Gradient Wave'),
-              onTap: this._navigateTo(context, 'gradient-page'),
+              title: Text('Headers Painters'),
+              onTap: this._navigateTo(context, 'painters-page'),
             ),
             ListTile(
               leading: Icon(Icons.waves_sharp),
@@ -56,7 +21,7 @@ class MenuPage extends StatelessWidget {
               onTap: this._navigateTo(context, 'animations-page'),
             ),
              ListTile(
-              leading: Icon(Icons.airplanemode_active),
+              leading: Icon(Icons.bubble_chart_outlined),
               title: Text('Cuadrado animado'),
               onTap: this._navigateTo(context, 'cuadrado-animado-page'),
             ),
@@ -69,6 +34,11 @@ class MenuPage extends StatelessWidget {
               leading: Icon(Icons.slideshow_rounded),
               title: Text('Slideshow'),
               onTap: this._navigateTo(context, 'slideshow-page'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_camera_sharp),
+              title: Text('Pinterest Page'),
+              onTap: this._navigateTo(context, 'pinterest-page'),
             ),
           ],
         ),
---
+++ lib/src/pages/pinterest_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class PinterestPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: PinterestGrid(),
+      ),
+    );
+  }
+}
+
+class PinterestGrid extends StatelessWidget {
+  final List items = List.generate(200, (index) => index);
+
+  PinterestGrid({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StaggeredGridView.countBuilder(
+      crossAxisCount: 4,
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) => _PinterestItem(index),
+      staggeredTileBuilder: (int index) =>
+          new StaggeredTile.count(2, index.isEven ? 2 : 3),
+      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4.0,
+    );
+  }
+}
+
+class _PinterestItem extends StatelessWidget {
+  final int index;
+
+  _PinterestItem(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        margin: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+        ),
+        child: new Center(
+          child: new CircleAvatar(
+            backgroundColor: Colors.white,
+            child: new Text('$index'),
+          ),
+        ));
+  }
+}
