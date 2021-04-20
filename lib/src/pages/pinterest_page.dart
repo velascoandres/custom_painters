8f0d56d23e211784972fadbbc0d6c839dd63ddaf
andres <velasco.andrs@gmail.com>
2021-04-20 17:20:43 -0500

    meno close to ready

--- lib/src/pages/pinterest_page.dart
+++ lib/src/pages/pinterest_page.dart
@@ -1,26 +1,88 @@
 import 'package:custom_painters/src/widgets/pinterest_meu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:provider/provider.dart';
 
 class PinterestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: PinterestMenu(),
+    return ChangeNotifierProvider(
+      create: (_) => _MenuModel(),
+      child: Scaffold(
+        // body: PinterestMenu()
+        body: Stack(
+          children: [
+            PinterestGrid(),
+            _PinterestMenuLocation(),
+          ],
+        ),
       ),
     );
   }
 }
 
-class PinterestGrid extends StatelessWidget {
-  final List items = List.generate(200, (index) => index);
+class _PinterestMenuLocation extends StatelessWidget {
+  const _PinterestMenuLocation({
+    Key key,
+  }) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<_MenuModel>(context);
+
+    return Positioned.fill(
+      bottom: 10,
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: PinterestMenu(
+          show: provider.show,
+          activeColor: Colors.red,
+          backgroundColor: Colors.amber,
+          inactiveColor: Colors.black45,
+        ),
+      ),
+    );
+  }
+}
+
+class PinterestGrid extends StatefulWidget {
   PinterestGrid({Key key}) : super(key: key);
+
+  @override
+  _PinterestGridState createState() => _PinterestGridState();
+}
+
+class _PinterestGridState extends State<PinterestGrid> {
+  final List items = List.generate(200, (index) => index);
+  ScrollController controller = new ScrollController();
+  double scrollAnterior = 0;
+
+  @override
+  void initState() {
+    final provider = Provider.of<_MenuModel>(context, listen: false);
+    controller.addListener(() {
+      if (controller.offset > scrollAnterior && controller.offset > 150) {
+        // Ocultar menu
+        provider.show = false;
+      } else {
+        // Mostrar menu
+        provider.show = true;
+      }
+      scrollAnterior = controller.offset;
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return StaggeredGridView.countBuilder(
+      controller: controller,
       crossAxisCount: 4,
       itemCount: items.length,
       itemBuilder: (BuildContext context, int index) => _PinterestItem(index),
@@ -52,5 +114,16 @@ class _PinterestItem extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class _MenuModel with ChangeNotifier {
+  bool _show = true;
+
+  bool get show => this._show;
+
+  set show(bool value) {
+    this._show = value;
+    notifyListeners();
   }
 }
--- lib/src/widgets/pinterest_meu.dart
+++ lib/src/widgets/pinterest_meu.dart
@@ -9,6 +9,12 @@ class PinterestMenuButton {
 }
 
 class PinterestMenu extends StatelessWidget {
+  final bool show;
+
+  final Color backgroundColor;
+  final Color activeColor;
+  final Color inactiveColor;
+
   final List<PinterestMenuButton> items = [
     PinterestMenuButton(
       icon: Icons.pie_chart,
@@ -36,12 +42,32 @@ class PinterestMenu extends StatelessWidget {
     ),
   ];
 
+  PinterestMenu({
+    Key key,
+    this.show = true,
+    this.backgroundColor = Colors.white,
+    this.activeColor = Colors.black,
+    this.inactiveColor = Colors.blueGrey,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => new _MenuModel(),
-      child: _PinterestMenuBackground(
-        child: _MenuItems(items),
+      child: Builder(
+        builder: (BuildContext context) {
+          final provider = Provider.of<_MenuModel>(context);
+          provider.backgroundColor = this.backgroundColor;
+          provider.activeColor = this.activeColor;
+          provider.inactiveColor = this.inactiveColor;
+          return AnimatedOpacity(
+            duration: Duration(milliseconds: 300),
+            opacity: show ? 1 : 0,
+            child: _PinterestMenuBackground(
+              child: _MenuItems(items),
+            ),
+          );
+        },
       ),
     );
   }
@@ -54,12 +80,13 @@ class _PinterestMenuBackground extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<_MenuModel>(context);
     return Container(
       child: this.child,
       width: 250,
       height: 60,
       decoration: BoxDecoration(
-        color: Colors.white,
+        color: provider.backgroundColor,
         borderRadius: BorderRadius.all(Radius.circular(100)),
         boxShadow: [
           BoxShadow(color: Colors.black38, blurRadius: 10, spreadRadius: -5),
@@ -94,13 +121,16 @@ class _PinterestMenuItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final itemSeleccionado = Provider.of<_MenuModel>(context).itemSeleccionado;
-    final color =  itemSeleccionado == index ? Colors.black : Colors.blueGrey;
-    final double size =  itemSeleccionado == index ? 30 : 25;
+    final provider = Provider.of<_MenuModel>(context);
+    final itemSeleccionado = provider.itemSeleccionado;
+    final color = itemSeleccionado == index ? provider.activeColor : provider.inactiveColor;
+
+    final double size = itemSeleccionado == index ? 30 : 25;
 
     return GestureDetector(
-      onTap: (){
-        Provider.of<_MenuModel>(context, listen: false).itemSeleccionado = index;
+      onTap: () {
+        Provider.of<_MenuModel>(context, listen: false).itemSeleccionado =
+            index;
         item.onPressed();
       },
       behavior: HitTestBehavior.translucent,
@@ -117,11 +147,33 @@ class _PinterestMenuItem extends StatelessWidget {
 
 class _MenuModel with ChangeNotifier {
   int _itemSeleccionado = 0;
+  Color _backgroundColor;
+  Color _activeColor;
+  Color _inactiveColor;
 
   int get itemSeleccionado => this._itemSeleccionado;
+
+  Color get backgroundColor => this._backgroundColor;
+  Color get activeColor => this._activeColor;
+  Color get inactiveColor => this._inactiveColor;
 
   set itemSeleccionado(int index) {
     this._itemSeleccionado = index;
     notifyListeners();
+  }
+
+  set backgroundColor(Color color) {
+    this._backgroundColor = color;
+    // notifyListeners();
+  }
+
+  set activeColor(Color color) {
+    this._activeColor = color;
+    // notifyListeners();
+  }
+
+  set inactiveColor(Color color) {
+    this._inactiveColor = color;
+    // notifyListeners();
   }
 }
