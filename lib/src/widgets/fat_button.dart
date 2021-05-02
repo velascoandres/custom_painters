8de307ef495cb99cbfbce08ec13ab656424782ef
Andrés Velasco <velasco.andrs@gmail.com>
2021-05-01 22:58:33 -0500

    informacion dentro del boton

--- lib/src/widgets/fat_button.dart
+++ lib/src/widgets/fat_button.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BotonGordo extends StatelessWidget {
   const BotonGordo({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return _BotonGordoBackground();
+    return Stack(
+      children: [
+        _BotonGordoBackground(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 100, width: 40),
+            FaIcon(FontAwesomeIcons.carCrash, color: Colors.white, size: 40),
+            SizedBox(width: 20),
+            Expanded(
+              child: Text('Motor Accident',
+                  style: TextStyle(color: Colors.white, fontSize: 18)),
+            ),
+            FaIcon(
+              FontAwesomeIcons.chevronRight,
+              color: Colors.white,
+            ),
+            SizedBox(width: 40),
+          ],
+        )
+      ],
+    );
   }
 }
 
@@ -17,6 +39,22 @@ class _BotonGordoBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Stack(
+          children: [
+            Positioned(
+              right: -20,
+              top: -20,
+              child: FaIcon(
+                FontAwesomeIcons.carCrash,
+                size: 150,
+                color: Colors.white.withOpacity(0.2),
+              ),
+            ),
+          ],
+        ),
+      ),
       width: double.infinity,
       height: 100,
       margin: EdgeInsets.symmetric(horizontal: 20),
