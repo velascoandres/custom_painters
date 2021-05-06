3b7b71b0f0fd7308c244abe8e106df17b30c8415
andres <velasco.andrs@gmail.com>
2021-05-05 19:38:12 -0500

    sliver complete

--- lib/src/pages/sliver_list_page.dart
+++ lib/src/pages/sliver_list_page.dart
@@ -1,9 +1,74 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+final items = [
+  _ListItem('Orange', Color(0xffF08F66)),
+  _ListItem('Family', Color(0xffF2A38A)),
+  _ListItem('Subscriptions', Color(0xffF7CDD5)),
+  _ListItem('Books', Color(0xffFCEBAF)),
+  _ListItem('Orange', Color(0xffF08F66)),
+  _ListItem('Family', Color(0xffF2A38A)),
+  _ListItem('Subscriptions', Color(0xffF7CDD5)),
+  _ListItem('Books', Color(0xffFCEBAF)),
+];
 
 class SliverListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: _Titulo());
+
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          _MainScroll(),
+          Positioned(
+            bottom: -10,
+            right: 0,
+            height: 100,
+            width: size.width * 0.9,
+            child: _BotonNewList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BotonNewList extends StatelessWidget {
+  const _BotonNewList({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return ButtonTheme(
+      height: 200,
+      minWidth: size.width,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50),
+              ),
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.all<Color>(
+            Color(0xffED6762),
+          ),
+        ),
+        onPressed: () {},
+        child: Text(
+          'CREATE NEW LIST',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 3,
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -15,11 +80,74 @@ class _ListaCosas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 20,
+      itemCount: items.length,
       itemBuilder: (ctx, index) {
-        return _ListItem();
+        return items[index];
       },
     );
+  }
+}
+
+class _MainScroll extends StatelessWidget {
+  const _MainScroll({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CustomScrollView(
+        slivers: [
+          SliverPersistentHeader(
+            floating: true,
+            delegate: _SliverCustomHeaderDelegate(
+              minHeight: 170,
+              maxHeight: 200,
+              child: Container(
+                alignment: Alignment.centerLeft,
+                color: Colors.white,
+                child: _Titulo(),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [...items, SizedBox(height: 100)],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  _SliverCustomHeaderDelegate({
+    @required this.minHeight,
+    @required this.maxHeight,
+    @required this.child,
+  });
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(
+      child: this.child,
+    );
+  }
+
+  @override
+  double get maxExtent => maxHeight;
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  bool shouldRebuild(covariant _SliverCustomHeaderDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight;
   }
 }
 
@@ -65,20 +193,25 @@ class _Titulo extends StatelessWidget {
 }
 
 class _ListItem extends StatelessWidget {
+  final String titulo;
+  final Color color;
+
+  const _ListItem(this.titulo, this.color);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(30),
       alignment: Alignment.centerLeft,
       child: Text(
-        'Orange',
+        titulo,
         style: TextStyle(
             color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
       ),
       height: 130,
       margin: EdgeInsets.all(10),
       decoration: BoxDecoration(
-        color: Colors.orange,
+        color: color,
         borderRadius: BorderRadius.circular(30),
       ),
     );
